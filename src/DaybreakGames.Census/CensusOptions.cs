3065ecf7ad0829d31358eb8abfcdd5c346af67bb
aBinaryStory <aBinaryStory@gmail.com>
2022-09-13 17:47:31 +0200

    Allow websocket endpoint to be configurable

--- src/DaybreakGames.Census/CensusOptions.cs
+++ src/DaybreakGames.Census/CensusOptions.cs
@@ -4,6 +4,7 @@
     {
         public string CensusServiceId { get; set; } = Constants.DefaultServiceId;
         public string CensusServiceNamespace { get; set; } = Constants.DefaultServiceNamespace;
+        public string CensusWebsocketEndpoint { get; set; } = Constants.CensusWebsocketEndpoint;
         public bool LogCensusErrors { get; set; } = false;
     }
 }
